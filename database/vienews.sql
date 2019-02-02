cf8be326278c7b470bc16b3ef25e9fc753c59d07
Saikat Singha Mahapatra <saikat.singhamahapatra@cognizant.com>
2019-02-02 11:24:59 +0530

    added fav

--- database/vienews.sql
+++ database/vienews.sql
@@ -2,24 +2,50 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
+DROP SCHEMA IF EXISTS `viewnews` ;
 CREATE SCHEMA IF NOT EXISTS `viewnews` DEFAULT CHARACTER SET latin1 ;
 USE `viewnews` ;
 
 -- -----------------------------------------------------
+-- Table `viewnews`.`article`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `viewnews`.`article` ;
+
+CREATE  TABLE IF NOT EXISTS `viewnews`.`article` (
+  `ar_id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `ar_us_id` INT(11) NOT NULL ,
+  `ar_author` VARCHAR(550) NULL DEFAULT NULL ,
+  `ar_title` VARCHAR(450) NOT NULL ,
+  `ar_publishedAt` VARCHAR(155) NULL DEFAULT NULL ,
+  `ar_content` TEXT NULL DEFAULT NULL ,
+  `ar_url` VARCHAR(550) NULL DEFAULT NULL ,
+  `ar_urlToImage` VARCHAR(550) NULL DEFAULT NULL ,
+  PRIMARY KEY (`ar_id`) ,
+  INDEX `as_us_fk` (`ar_us_id` ASC) )
+ENGINE = InnoDB
+AUTO_INCREMENT = 18
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
 -- Table `viewnews`.`language`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `viewnews`.`language` ;
+
 CREATE  TABLE IF NOT EXISTS `viewnews`.`language` (
   `la_id` INT(11) NOT NULL AUTO_INCREMENT ,
   `la_name` VARCHAR(200) NOT NULL ,
   PRIMARY KEY (`la_id`) )
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
+AUTO_INCREMENT = 5
 DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `viewnews`.`role`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `viewnews`.`role` ;
+
 CREATE  TABLE IF NOT EXISTS `viewnews`.`role` (
   `ro_id` INT(11) NOT NULL AUTO_INCREMENT ,
   `ro_name` VARCHAR(95) NOT NULL ,
@@ -32,6 +58,8 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `viewnews`.`user`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `viewnews`.`user` ;
+
 CREATE  TABLE IF NOT EXISTS `viewnews`.`user` (
   `us_id` INT(11) NOT NULL AUTO_INCREMENT ,
   `us_name` VARCHAR(80) NOT NULL ,
@@ -55,32 +83,7 @@ CREATE  TABLE IF NOT EXISTS `viewnews`.`user` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 9
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `viewnews`.`article`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `viewnews`.`article` (
-  `ar_id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `ar_us_id` INT(11) NULL DEFAULT NULL ,
-  `ar_language` INT(11) NOT NULL ,
-  `ar_author` VARCHAR(250) NOT NULL ,
-  `ar_title` VARCHAR(450) NOT NULL ,
-  `ar_description` TEXT NOT NULL ,
-  `ar_publishedAt` VARCHAR(155) NULL DEFAULT NULL ,
-  `ar_content` TEXT NULL DEFAULT NULL ,
-  `ar_url` VARCHAR(150) NULL DEFAULT NULL ,
-  `ar_urlToImage` VARCHAR(250) NULL DEFAULT NULL ,
-  PRIMARY KEY (`ar_id`) ,
-  INDEX `as_us_fk` (`ar_us_id` ASC) ,
-  CONSTRAINT `as_us_fk`
-    FOREIGN KEY (`ar_us_id` )
-    REFERENCES `viewnews`.`user` (`us_id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
+AUTO_INCREMENT = 18
 DEFAULT CHARACTER SET = latin1;
 
 
